02463486ba34e5cf0b4e51e9c85edbc5a39ae542
Andrew Vos <andrew.vos@gmail.com>
2011-04-20 23:18:00 +0100

    Fixed a flaky test then decided to remove the Interval property! I figured that because the firefox browser runs in a different thread that we don't have to have an interval. Should review this tomorrow though!

--- src/Coypu/Robustness/RobustWrapper.cs
+++ src/Coypu/Robustness/RobustWrapper.cs
@@ -5,7 +5,6 @@ namespace Coypu.Robustness
 	public interface RobustWrapper
 	{
 		TimeSpan Timeout { get; }
-		TimeSpan Interval { get; }
 		void Robustly(Action action);
 		TResult Robustly<TResult>(Func<TResult> function);
 	}
