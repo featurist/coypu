8b38bec90d71be85348820bd2b4d8c06c02f9d85
= <adrian.longley@gmail.com>
2011-04-21 09:53:03 +0100

    Fix interval @ 10% of timeout. Close driver on dispose (for IE).

--- src/Coypu/Robustness/WaitAndRetryRobustWrapper.cs
+++ src/Coypu/Robustness/WaitAndRetryRobustWrapper.cs
@@ -14,26 +14,16 @@ namespace Coypu.Robustness
 
 		public void Robustly(Action action)
 		{
-			var startTime = DateTime.Now;
-			while (true)
-			{
-				try
-				{
-					action();
-					break;
-				}
-				catch (Exception)
-				{
-					if (DateTime.Now - startTime >= Timeout)
-					{
-						throw;
-					}
-				}
-			}
+			Robustly<object>(() =>
+			         	{
+			         		action();
+			         		return null;
+			         	});
 		}
 
 		public TResult Robustly<TResult>(Func<TResult> function)
 		{
+			var interval = (int) Math.Round(Timeout.TotalMilliseconds / 10);
 			var startTime = DateTime.Now;
 			while (true)
 			{
@@ -47,6 +37,7 @@ namespace Coypu.Robustness
 					{
 						throw;
 					}
+					Thread.Sleep(interval);
 				}
 			}
 		}
