df6542db81ae5c9480a2e0b776a5bbeddf234da8
= <adrian.longley@gmail.com>
2011-05-02 20:32:45 +0100

    Started WatiN driver

---
+++ src/Coypu/Drivers/WatiNDriver/WatiNExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using OpenQA.Selenium;
+
+namespace Coypu.Drivers.WatiNDriver
+{
+	public static class WatiNExtensions
+	{
+		public static bool Displayed(this object webElement)
+		{
+			throw new NotImplementedException();
+//			var renderedElement = webElement as IRenderedWebElement;
+//			return renderedElement != null && renderedElement.Displayed;
+		}
+
+	}
+}
