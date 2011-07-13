caffd0799ec35b781e3a0da8489263dc784b1c23
Adrian Longley <adrian.longley@gmail.com>
2011-07-13 17:22:17 +0100

    Started acceptance tests for Web Resource

---
+++ src/Coypu.AcceptanceTests/sites/site_with_secure_resources.rb
@@ -0,0 +1,15 @@
+require 'sinatra'
+
+get "/resource/:value" do
+  params[:value]
+end
+
+get "/auto_login/?" do
+  response.set_cookie "username", {:value => "bob"}
+end
+
+get "/restricted_resource/:value" do
+  if request.cookies["username"] == "bob"
+    params[:value]
+  end
+end
