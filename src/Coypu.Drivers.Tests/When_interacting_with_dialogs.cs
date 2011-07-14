7a78f3c728340a204adf94361e6404f733ad1725
Adrian Longley <adrian.longley@gmail.com>
2011-07-14 17:22:29 +0100

    Completed SaveWebResource which saves a resource from the web directly using the current browser session's cookies

--- src/Coypu.Drivers.Tests/When_interacting_with_dialogs.cs
+++ src/Coypu.Drivers.Tests/When_interacting_with_dialogs.cs
@@ -9,13 +9,13 @@ namespace Coypu.Drivers.Tests
         {
             it["accepts alerts"] = () => 
             {
-                DisposeOnError(() =>
+                using(driver)
                 {
                     driver.Click(driver.FindLink("Trigger an alert"));
                     driver.HasDialog("You have triggered an alert and this is the text.").should_be_true();
                     driver.AcceptModalDialog();
                     driver.HasDialog("You have triggered an alert and this is the text.").should_be_false();
-                });
+                };
             };
 
             describe["confirms"] = () =>
@@ -24,60 +24,47 @@ namespace Coypu.Drivers.Tests
                 {
                     it["clears dialog"] = () =>
                     {
-                        DisposeOnError(() => 
+                        using(driver)
                         {
                             driver.Click(driver.FindLink("Trigger a confirm"));
                             driver.HasDialog("You have triggered a confirm and this is the text.").should_be_true();
                             driver.AcceptModalDialog();
                             driver.HasDialog("You have triggered a confirm and this is the text.").should_be_false();
-                        });
+                        };
                     };
                     it["returns true"] = () =>
                     {
-                        DisposeOnError(() => 
+                        using(driver)
                         {
                             driver.Click(driver.FindLink("Trigger a confirm"));
                             driver.AcceptModalDialog();
                             driver.FindLink("Trigger a confirm - accepted").should_not_be_null();
-                        });
+                        };
                     };
                 };
                 describe["when cancelling"] = () =>
                 {
                     it["clears dialog"] = () =>
                     {
-                        DisposeOnError(() =>
+                        using(driver)
                         {
                             driver.Click(driver.FindLink("Trigger a confirm"));
                             driver.HasDialog("You have triggered a confirm and this is the text.").should_be_true();
                             driver.CancelModalDialog();
                             driver.HasDialog("You have triggered a confirm and this is the text.").should_be_false();
-                        });
+                        };
                     };
                     it["returns false"] = () =>
                     {
-                        DisposeOnError(() =>
+                        using(driver)
                         {
                             driver.Click(driver.FindLink("Trigger a confirm"));
                             driver.CancelModalDialog();
                             driver.FindLink("Trigger a confirm - cancelled").should_not_be_null();
-                        });
+                        };
                     };
                 };
             };
-        }
-
-        void DisposeOnError(Action action)
-        {
-            try
-            {
-                action();
-            }
-            catch (Exception)
-            {
-                driver.Dispose();
-                throw;
-            }
         }
     }
 }
--- src/Coypu/Drivers/Selenium/OptionSelector.cs
+++ src/Coypu/Drivers/Selenium/OptionSelector.cs
@@ -14,7 +14,7 @@ namespace Coypu.Drivers.Selenium
             if (optionToSelect == null)
                 throw new MissingHtmlException("No such option: " + option);
 
-            optionToSelect.Click();
+            optionToSelect.Select();
         }
     }
 }
