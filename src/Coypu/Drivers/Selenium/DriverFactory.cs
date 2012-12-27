74741cda482d6c52907b9dc55cbb94119bdbf521
Geoff McElhanon <geoffrey@mcelhanon.com>
2012-12-27 17:26:55 -0600

    Added support for PhantomJS and provided ability to pass explicit browser-specific options through to Selenium web driver implementations.

--- src/Coypu/Drivers/Selenium/DriverFactory.cs
+++ src/Coypu/Drivers/Selenium/DriverFactory.cs
@@ -1,25 +1,51 @@
 using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.IE;
 using OpenQA.Selenium.Android;
+using OpenQA.Selenium.PhantomJS;
 using OpenQA.Selenium.Remote;
 
 namespace Coypu.Drivers.Selenium
 {
     internal class DriverFactory
     {
-        public IWebDriver NewWebDriver(Browser browser)  {
+        public IWebDriver NewWebDriver(Browser browser) {
+            return NewWebDriver(browser, new Dictionary<Browser, object>());
+        }
+
+        public IWebDriver NewWebDriver(Browser browser, IDictionary<Browser, object> browserOptions)  {
             if (browser == Browser.Firefox)
                 return new FirefoxDriver();
             if (browser == Browser.InternetExplorer)
             {
-                var options = new InternetExplorerOptions
+                InternetExplorerOptions options;
+
+                if (browserOptions.ContainsKey(Browser.InternetExplorer))
+                {
+                    options = browserOptions[Browser.InternetExplorer] as InternetExplorerOptions;
+
+                    // Make sure options provided are of the expected type for Internet Explorer
+                    if (options == null)
                     {
-                        IntroduceInstabilityByIgnoringProtectedModeSettings = true,
-                        EnableNativeEvents = true
-                    };
+                        throw new ArgumentException(
+                            "Options object provided for Internet Explorer was not the expected type (Selenium's InternetExplorerOptions).");
+                    }
+                }
+                else
+                {
+                    // Default options
+                    options = new InternetExplorerOptions
+                                  {
+                                      IntroduceInstabilityByIgnoringProtectedModeSettings = true,
+                                      EnableNativeEvents = true
+                                  };
+                }
+
                 return new InternetExplorerDriver(options);
             }
             if (browser == Browser.Chrome)
@@ -33,7 +59,53 @@ namespace Coypu.Drivers.Selenium
                 desiredCapabilities.IsJavaScriptEnabled = true;
                 return new RemoteWebDriver(desiredCapabilities);
             }
+            if (browser == Browser.PhantomJS)
+                return GetPhantomJSDriver(browserOptions);
+
             return browserNotSupported(browser,null);
+        }
+
+        private static IWebDriver GetPhantomJSDriver(IDictionary<Browser, object> browserOptions)
+        {
+            if (browserOptions.ContainsKey(Browser.PhantomJS))
+            {
+                // Try processing options as pre-initialized PhantomJSDriverService from Selenium
+                var driverService = browserOptions[Browser.PhantomJS] as PhantomJSDriverService;
+
+                // PhantomJSDriverService was not provided?
+                if (driverService == null)
+                {
+                    // Try processing options as JSON configuration for PhantomJS
+                    string jsonOptions = browserOptions[Browser.PhantomJS] as string;
+
+                    if (jsonOptions != null)
+                    {
+                        try
+                        {
+                            // Try deserializing JSON into a default driver service instance
+                            driverService = JsonConvert.DeserializeObject<PhantomJSDriverService>(jsonOptions);
+                        }
+                        catch (Exception ex)
+                        {
+                            throw new SerializationException(
+                                "Unable to deserialize the string value supplied as the browser options for PhantomJS into a PhantomJSDriverService instance.  Please review the PhantomJS documentation for JSON configuration, or serialize an instance of a PhantomJSDriverService instance.",
+                                ex);
+                        }
+                    }
+                }
+
+                // Make sure options provided are of the expected type for PhantomJS
+                if (driverService == null)
+                {
+                    throw new ArgumentException(
+                        "The browser options provided for PhantomJS were not the expected type (Selenium's PhantomJSDriverService, or a JSON string containing the configuration, as specified in the PhantomJS command-line arguments documentation).");
+                }
+
+                return new PhantomJSDriver(driverService);
+            }
+
+            // Default to PhantomJS with no specific options
+            return new PhantomJSDriver();
         }
 
         private IWebDriver browserNotSupported(Browser browser, Exception inner) {
