55d34d04820ad50c36060a636dc6785a2b9f79c4
Nicholas Mayne <Jetski5822@gmail.com>
2013-02-15 12:34:27 +0000

    Checkboxes can be found by their value

--- src/Coypu.Drivers.Tests/When_finding_fields_by_value.cs
+++ src/Coypu.Drivers.Tests/When_finding_fields_by_value.cs
@@ -6,10 +6,15 @@ namespace Coypu.Drivers.Tests
     internal class When_finding_fields_by_value : DriverSpecs
     {
         [Test]
-        public void Finds_radio_button_by_value()
-        {
+        public void Finds_radio_button_by_value() {
             Driver.FindField("radio field one val", Root).Name.should_be("forLabeledRadioFieldName");
             Driver.FindField("radio field two val", Root).Name.should_be("containerLabeledRadioFieldName");
+        }
+
+        [Test]
+        public void Finds_checkbox_by_value() {
+            Driver.FindField("checkbox one val", Root).Name.should_be("checkboxByValueOneFieldName");
+            Driver.FindField("checkbox two val", Root).Name.should_be("checkboxByValueTwoFieldName");
         }
 
     }
