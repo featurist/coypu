64b04ab765d54a6d826271f7fd9b52c08ef7f519
Martavis Parker <martavis@accuauto.net>
2013-05-14 11:17:07 -0400

    Added new retry strategy that swallows MissingHtmlException.
    Use case: featurist/coypu#46

---
+++ src/Coypu/Robustness/RetryUntilTimeoutThenSwallowRobustWrapper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Coypu.Robustness
+{
+    using Coypu.Actions;
+    using Coypu.Queries;
+
+    public class RetryUntilTimeoutThenSwallowRobustWrapper : RetryUntilTimeoutRobustWrapper
+    {
+        public override void TryUntil(BrowserAction tryThis, PredicateQuery until, TimeSpan overrallTimeout, TimeSpan waitBeforeRetry)
+        {
+            try
+            {
+                Robustly(new ActionSatisfiesPredicateQuery(tryThis, until, overrallTimeout, until.RetryInterval, waitBeforeRetry, this));
+            }
+            catch (MissingHtmlException)
+            {   
+            }
+        }
+    }
+}
