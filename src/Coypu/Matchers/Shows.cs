5ed0b936158036bf6bbbe778b0db201ed5f3deba
Adrian Longley <adrian.longley@gmail.com>
2013-10-18 13:14:17 +0100

    Add some overloads to new matchers to support params args

--- src/Coypu/Matchers/Shows.cs
+++ src/Coypu/Matchers/Shows.cs
@@ -29,34 +29,59 @@ namespace Coypu.Matchers {
             return new HasCssMatcher(expectedCssSelector, text, options);
         }
 
-        public static Constraint ContentContaining(IEnumerable<string> expectedContent, Options options = null)
+        public static Constraint ContentContaining(params string[] expectedContent)
+        {
+            return new AggregateMatcher<HasContentMatcher>(
+                    expectedContent.Select(c => new HasContentMatcher(c, null)));
+        }
+
+        public static Constraint ContentContaining(IEnumerable<string> expectedContent, Options options)
         {
             return new AggregateMatcher<HasContentMatcher>(
                     expectedContent.Select(c => new HasContentMatcher(c, options)));
         }
 
-        public static Constraint CssContaining(string expectedCssSelector, IEnumerable<string> text, Options options = null)
+        public static Constraint CssContaining(string expectedCssSelector, params string[] text)
+        {
+            return CssContaining(expectedCssSelector, text, null);
+        }
+
+        public static Constraint CssContaining(string expectedCssSelector, IEnumerable<string> text, Options options)
         {
             return new AggregateMatcher<HasCssMatcher>(
                     text.Select(c => new HasCssMatcher(expectedCssSelector, c, options)));
         }
 
-        public static Constraint CssContaining(string expectedCssSelector, IEnumerable<Regex> text, Options options = null)
+        public static Constraint CssContaining(string expectedCssSelector, Regex[] text)
+        {
+            return CssContaining(expectedCssSelector, text);
+        }
+
+        public static Constraint CssContaining(string expectedCssSelector, IEnumerable<Regex> text, Options options)
         {
             return new AggregateMatcher<HasCssMatcher>(
                     text.Select(c => new HasCssMatcher(expectedCssSelector, c, options)));
         }
 
-        public static IResolveConstraint AllCssInOrder(string expectedCssSelector, IEnumerable<string> text, Options options = null)
+        public static IResolveConstraint AllCssInOrder(string expectedCssSelector, params string[] text)
+        {
+            return AllCssInOrder(expectedCssSelector, text, null);
+        }
+
+        public static IResolveConstraint AllCssInOrder(string expectedCssSelector, IEnumerable<string> text, Options options)
         {
             return new HasAllCssInOrderMatcher(expectedCssSelector, text, options);
+        }
+
+        public static IResolveConstraint AllCssInOrder(string expectedCssSelector, params Regex[] textMatching)
+        {
+            return AllCssInOrder(expectedCssSelector, textMatching, null);
         }
 
         public static IResolveConstraint AllCssInOrder(string expectedCssSelector, IEnumerable<Regex> textMatching, Options options = null)
         {
             return new HasAllCssInOrderMatcher(expectedCssSelector, textMatching, options);
         }
-
 
         public static ShowsNo No = new ShowsNo();
 
