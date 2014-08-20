e4a92d780a20925c9aac7f222bef1c3ee95d1511
Adrian Longley <adrian.longley@gmail.com>
2014-08-20 18:38:36 +0100

    Fix SelfishSite urls in DriverTests

--- src/Coypu.Drivers.Tests/When_navigating.cs
+++ src/Coypu.Drivers.Tests/When_navigating.cs
@@ -6,20 +6,19 @@ namespace Coypu.Drivers.Tests
 {
     internal class When_inspecting_location : DriverSpecs
     {
-
         [Test]
         public void Go_back_and_forward_in_history()
         {
             using (Driver)
             {
-                Driver.Visit("http://localhost:4567", Root);
-                Driver.Visit("http://localhost:4567/auto_login", Root);
+                Driver.Visit(TestSiteUrl("/"), Root);
+                Driver.Visit(TestSiteUrl("/auto_login"), Root);
 
                 Driver.GoBack(Root);
-                Assert.That(Driver.Location(Root), Is.EqualTo(new Uri("http://localhost:4567/")));
+                Assert.That(Driver.Location(Root), Is.EqualTo(new Uri(TestSiteUrl("/"))));
 
                 Driver.GoForward(Root);
-                Assert.That(Driver.Location(Root), Is.EqualTo(new Uri("http://localhost:4567/auto_login")));
+                Assert.That(Driver.Location(Root), Is.EqualTo(new Uri(TestSiteUrl("/auto_login"))));
 
             }
         }
@@ -33,20 +32,20 @@ namespace Coypu.Drivers.Tests
                 var popUp = new DriverScope(DefaultSessionConfiguration, new WindowFinder(Driver, "Pop Up Window", Root, DefaultOptions),
                                             Driver, null, null, null, DisambiguationStrategy);
 
-                Driver.Visit("http://localhost:4567/auto_login", Root);
-                Driver.Visit("http://localhost:4567", popUp);
+                Driver.Visit(TestSiteUrl("/auto_login"), Root);
+                Driver.Visit(TestSiteUrl("/"), popUp);
 
                 Driver.GoBack(popUp);
                 Assert.That(Driver.Location(popUp).AbsoluteUri,
                             Is.StringEnding("src/Coypu.Drivers.Tests/html/popup.htm"));
-                Assert.That(Driver.Location(Root).AbsoluteUri, Is.EqualTo("http://localhost:4567/auto_login"));
+                Assert.That(Driver.Location(Root).AbsoluteUri, Is.EqualTo(TestSiteUrl("/auto_login")));
 
                 Driver.GoForward(popUp);
-                Assert.That(Driver.Location(popUp).AbsoluteUri, Is.EqualTo("http://localhost:4567/"));
+                Assert.That(Driver.Location(popUp).AbsoluteUri, Is.EqualTo(TestSiteUrl("/")));
 
                 Driver.GoBack(Root);
                 Assert.That(Driver.Location(Root).AbsoluteUri, Is.StringEnding("/html/InteractionTestsPage.htm"));
-                Assert.That(Driver.Location(popUp).AbsoluteUri, Is.EqualTo("http://localhost:4567/"));
+                Assert.That(Driver.Location(popUp).AbsoluteUri, Is.EqualTo(TestSiteUrl("/")));
             }
         }
     }
