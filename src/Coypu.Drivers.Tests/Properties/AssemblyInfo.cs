c577e53a27c58b09ac9db3b07026fc8ef83ea01b
Adrian Longley <adrian.longley@gmail.com>
2014-12-03 09:03:42 +0000

    Bump to 2.6.0.1

--- src/Coypu.Drivers.Tests/Properties/AssemblyInfo.cs
+++ src/Coypu.Drivers.Tests/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2.6.0.0")]
-[assembly: AssemblyFileVersion("2.6.0.0")]
+[assembly: AssemblyVersion("2.6.0.1")]
+[assembly: AssemblyFileVersion("2.6.0.1")]
--- src/Coypu/Properties/AssemblyInfo.cs
+++ src/Coypu/Properties/AssemblyInfo.cs
@@ -38,7 +38,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2.6.0.0")]
-[assembly: AssemblyFileVersion("2.6.0.0")]
+[assembly: AssemblyVersion("2.6.0.1")]
+[assembly: AssemblyFileVersion("2.6.0.1")]
 [assembly: InternalsVisibleTo("Coypu.Tests")]
 [assembly: InternalsVisibleTo("Coypu.Drivers.Tests")]
