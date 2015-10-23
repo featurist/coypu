9ec780b94ed8d10020cbff242f132f48f6f38011
Adrian Longley <adrian.longley@gmail.com>
2015-10-23 09:53:48 +0100

    Upgrade to Selenium 2.48

--- src/Coypu.AcceptanceTests/WaitAndRetryExamples.cs
+++ src/Coypu.AcceptanceTests/WaitAndRetryExamples.cs
@@ -16,7 +16,7 @@ namespace Coypu.AcceptanceTests
             var configuration = new SessionConfiguration
                 {
                     Timeout = TimeSpan.FromMilliseconds(2000),
-                    Browser = Drivers.Browser.Chrome,
+                    Browser = Drivers.Browser.Firefox,
                     Driver = typeof(SeleniumWebDriver)
                 };
             browser = new BrowserSession(configuration);
