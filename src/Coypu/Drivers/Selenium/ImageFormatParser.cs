f7d6f71fe3ad4451681f5ee12ddf50ba43bcec37
Nicholas Mayne <Jetski5822@gmail.com>
2017-02-17 15:33:11 +0000

    updates

--- src/Coypu/Drivers/Selenium/ImageFormatParser.cs
+++ src/Coypu/Drivers/Selenium/ImageFormatParser.cs
@@ -1,33 +1,34 @@
 ﻿using System;
 using System.Drawing.Imaging;
 using System.IO;
+using OpenQA.Selenium;
 
 namespace Coypu.Drivers.Selenium
 {
     internal class ImageFormatParser
     {
-        internal static ImageFormat GetImageFormat(string fileName)
+        internal static ScreenshotImageFormat GetImageFormat(string fileName)
         {
             var extension = new FileInfo(fileName).Extension.ToLower().Replace("jpg", "jpeg");
 
-            ImageFormat format;
-            if      (AreEqual(extension, ImageFormat.Bmp))
-                format = ImageFormat.Bmp;
-            else if (AreEqual(extension, ImageFormat.Gif))
-                format = ImageFormat.Gif;
-            else if (AreEqual(extension, ImageFormat.Jpeg))
-                format = ImageFormat.Jpeg;
-            else if (AreEqual(extension, ImageFormat.Png))
-                format = ImageFormat.Png;
-            else if (AreEqual(extension, ImageFormat.Bmp))
-                format = ImageFormat.Bmp;
+            ScreenshotImageFormat format;
+            if  (AreEqual(extension, ScreenshotImageFormat.Bmp))
+                format = ScreenshotImageFormat.Bmp;
+            else if (AreEqual(extension, ScreenshotImageFormat.Gif))
+                format = ScreenshotImageFormat.Gif;
+            else if (AreEqual(extension, ScreenshotImageFormat.Jpeg))
+                format = ScreenshotImageFormat.Jpeg;
+            else if (AreEqual(extension, ScreenshotImageFormat.Png))
+                format = ScreenshotImageFormat.Png;
+            else if (AreEqual(extension, ScreenshotImageFormat.Bmp))
+                format = ScreenshotImageFormat.Bmp;
             else
-                format = ImageFormat.Jpeg;
+                format = ScreenshotImageFormat.Jpeg;
 
             return format;
         }
 
-        private static bool AreEqual(string extension, ImageFormat imageFormat)
+        private static bool AreEqual(string extension, ScreenshotImageFormat imageFormat)
         {
             return extension.Equals(imageFormat.ToString(), StringComparison.InvariantCultureIgnoreCase);
         }
