c3f3d861c21d32d728d8abd928de77a357bab3d2
Jakub Obstarczyk <jakub.obstarczyk@gmail.com>
2018-04-22 10:54:51 +0100

    removing Safari from the browser list - safaridriver i nto supported by Apple anymore

--- src/Coypu/Drivers/Browser.cs
+++ src/Coypu/Drivers/Browser.cs
@@ -2,32 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+#pragma warning disable 1591
 
 namespace Coypu.Drivers
 {
     /// <summary>
-    /// The browser that will be used by your chosen driver
+    ///     The browser that will be used by your chosen driver
     /// </summary>
     public class Browser
     {
-        private Browser(){}
+        public static Browser Firefox = new Browser
+                                        {
+                                            Javascript = true,
+                                            UppercaseTagNames = true
+                                        };
+
+        public static Browser InternetExplorer = new Browser {Javascript = true};
+        public static Browser Chrome = new Browser {Javascript = true};
+        public static Browser HtmlUnit = new Browser {Javascript = false};
+        public static Browser HtmlUnitWithJavaScript = new Browser {Javascript = true};
+        public static Browser PhantomJs = new Browser {Javascript = true};
+        public static Browser MicrosoftEdge = new Browser {Javascript = true};
+        public static Browser Opera = new Browser {Javascript = true};
+        private Browser() { }
 
         public bool Javascript { get; private set; }
         public bool UppercaseTagNames { get; private set; }
 
-        public static Browser Firefox                = new Browser { Javascript = true, UppercaseTagNames = true };
-        public static Browser InternetExplorer       = new Browser { Javascript = true };
-        public static Browser Chrome                 = new Browser { Javascript = true };
-        public static Browser Safari                 = new Browser { Javascript = true };
-        public static Browser HtmlUnit               = new Browser { Javascript = false };
-        public static Browser HtmlUnitWithJavaScript = new Browser { Javascript = true };
-        public static Browser PhantomJS              = new Browser { Javascript = true };
-        public static Browser MicrosoftEdge          = new Browser { Javascript = true };
-        public static Browser Opera                  = new Browser { Javascript = true };
-
         public static Browser Parse(string browserName)
         {
-            var fieldInfo = BrowserFields().FirstOrDefault(f => f.Name.Equals(browserName.Replace(" ",""), StringComparison.InvariantCultureIgnoreCase));
+            var fieldInfo = BrowserFields()
+                .FirstOrDefault(f => f.Name.Equals(browserName.Replace(" ", ""),
+                                                   StringComparison.InvariantCultureIgnoreCase));
             if (fieldInfo == null)
                 throw new NoSuchBrowserException(browserName);
 
--- src/Coypu/Drivers/Selenium/DriverFactory.cs
+++ src/Coypu/Drivers/Selenium/DriverFactory.cs
@@ -4,47 +4,52 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Edge;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Opera;
 using OpenQA.Selenium.PhantomJS;
 using OpenQA.Selenium.Remote;
-using OpenQA.Selenium.Opera;
 
 namespace Coypu.Drivers.Selenium
 {
     internal class DriverFactory
     {
-        public IWebDriver NewWebDriver(Browser browser)  {
+        public IWebDriver NewWebDriver(Browser browser)
+        {
             if (browser == Browser.Firefox)
                 return new FirefoxDriver();
             if (browser == Browser.InternetExplorer)
             {
                 var options = new InternetExplorerOptions
-                {
-                        IntroduceInstabilityByIgnoringProtectedModeSettings = true,
-                        EnableNativeEvents = true,
-                        IgnoreZoomLevel = true
-                    };
+                              {
+                                  IntroduceInstabilityByIgnoringProtectedModeSettings = true,
+                                  EnableNativeEvents = true,
+                                  IgnoreZoomLevel = true
+                              };
                 return new InternetExplorerDriver(options);
             }
+
             if (browser == Browser.Chrome)
                 return new ChromeDriver();
             if (browser == Browser.HtmlUnit)
                 return new RemoteWebDriver(DesiredCapabilities.HtmlUnit());
-            if (browser == Browser.HtmlUnitWithJavaScript) {
-                DesiredCapabilities desiredCapabilities = DesiredCapabilities.HtmlUnit();
+            if (browser == Browser.HtmlUnitWithJavaScript)
+            {
+                var desiredCapabilities = DesiredCapabilities.HtmlUnit();
                 desiredCapabilities.SetCapability(CapabilityType.IsJavaScriptEnabled, true);
                 return new RemoteWebDriver(desiredCapabilities);
             }
-            if (browser == Browser.PhantomJS)
+
+            if (browser == Browser.PhantomJs)
                 return new PhantomJSDriver();
             if (browser == Browser.MicrosoftEdge)
                 return new EdgeDriver();
-            if (browser == Browser.Opera)
-                return new OperaDriver();
-
-            return BrowserNotSupported(browser,null);
+            return browser == Browser.Opera
+                       ? new OperaDriver()
+                       : BrowserNotSupported(browser, null);
         }
 
-        private IWebDriver BrowserNotSupported(Browser browser, Exception inner) {
+        private IWebDriver BrowserNotSupported(Browser browser,
+                                               Exception inner)
+        {
             throw new BrowserNotSupportedException(browser, GetType(), inner);
         }
     }
