fd8af391ab9e5cad1e030cc6c19c037479f9ba07
Mikhail Mezhenin <mikhail.mezhenin@arcadia.spb.ru>
2018-06-09 17:59:28 +0300

    Firefox hover fix.

--- src/Coypu/Actions/Hover.cs
+++ src/Coypu/Actions/Hover.cs
@@ -15,13 +15,6 @@ namespace Coypu.Actions
         public override void Act()
         {
             var element = driverScope.Now();
-
-            // In Firefox, scrolling to an element in firefox is broken.
-            // Bug: https://github.com/mozilla/geckodriver/issues/901
-            if (driverScope.Browser == Browser.Firefox)
-            {
-                Driver.ExecuteScript("window.scrollTo(0, document.body.scrollHeight);", driverScope);
-            }
             Driver.Hover(element);
         }
     }
