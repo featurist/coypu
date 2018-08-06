240bc25bd2e663c477c72a7eebf4fecbed2abd19
Mikhail Mezhenin <mikhail.mezhenin@arcadia.spb.ru>
2018-08-06 10:49:11 +0300

    HasSession interface introduced

---
+++ src/Coypu/EM/HasSession.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Coypu
+{
+    public interface HasSession
+    {
+        void SetScope(BrowserSession s);
+    }
+}
