59a1eb0021c98e3f9e7b13be99582febe76a5fd0
Mikhail Mezhenin <mikhail.mezhenin@arcadia.spb.ru>
2018-08-20 18:03:47 +0300

    Updated Selenium to 3.14.0; removed PhantomJS dependency (obsolete in C# bindings), removed HtmlUnit (couldn't figure out a way to set it up properly now that DesiredCapabilities are obsolete).

--- src/Coypu/Drivers/Selenium/DriverFactory.cs
+++ src/Coypu/Drivers/Selenium/DriverFactory.cs
@@ -4,7 +4,6 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Edge;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.IE;
-using OpenQA.Selenium.PhantomJS;
 using OpenQA.Selenium.Remote;
 using OpenQA.Selenium.Opera;
 
@@ -27,15 +26,6 @@ namespace Coypu.Drivers.Selenium
             }
             if (browser == Browser.Chrome)
                 return new ChromeDriver();
-            if (browser == Browser.HtmlUnit)
-                return new RemoteWebDriver(DesiredCapabilities.HtmlUnit());
-            if (browser == Browser.HtmlUnitWithJavaScript) {
-                DesiredCapabilities desiredCapabilities = DesiredCapabilities.HtmlUnit();
-                desiredCapabilities.SetCapability(CapabilityType.IsJavaScriptEnabled, true);
-                return new RemoteWebDriver(desiredCapabilities);
-            }
-            if (browser == Browser.PhantomJS)
-                return new PhantomJSDriver();
             if (browser == Browser.MicrosoftEdge)
                 return new EdgeDriver();
             if (browser == Browser.Opera)
