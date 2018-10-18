1f70d1fe541df7b3cf5f98797dc8f1d6759b1182
Jakub Obstarczyk <jakub.obstarczyk@ding.com>
2018-10-18 13:13:24 +0100

    converting Coypu.NUnit.csproj to new vs2017 format

--- src/Coypu.NUnit/Properties/AssemblyInfo.cs
+++ src/Coypu.NUnit/Properties/AssemblyInfo.cs
@@ -2,18 +2,6 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Coypu.NUnit")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Coypu.NUnit")]
-[assembly: AssemblyCopyright("Copyright © Adrian Longley and contributors 2011-2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: CLSCompliant(true)]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -23,16 +11,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("20435fbd-3c57-4893-aa57-5406f4e6d35f")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.11.0.0")]
-[assembly: AssemblyFileVersion("3.11.0.0")]
