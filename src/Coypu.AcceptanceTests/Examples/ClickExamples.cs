ef7ae8007ca6690ee6ad7c877493008b5136a93f
Jakub Obstarczyk <jakub.obstarczyk@gmail.com>
2018-12-06 21:01:33 +0000

    added new tests ClickWaitBeforeClick() and ClickButtonWaitBeforeClick()

--- src/Coypu.AcceptanceTests/Examples/ClickExamples.cs
+++ src/Coypu.AcceptanceTests/Examples/ClickExamples.cs
@@ -1,11 +1,13 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Diagnostics;
+using NUnit.Framework;
 
 namespace Coypu.AcceptanceTests.Examples
 {
     public class ClickExamples : WaitAndRetryExamples
     {
         [Test]
-        public void Click_example()
+        public void Click()
         {
             var element = Browser.FindButton("clickMeTest");
             Assert.That(Browser.FindButton("clickMeTest")
@@ -19,7 +21,7 @@ namespace Coypu.AcceptanceTests.Examples
         }
 
         [Test]
-        public void ClickButton_example()
+        public void ClickButton()
         {
             Browser.ClickButton("clickMeTest");
             Assert.That(Browser.FindButton("clickMeTest")
@@ -28,17 +30,52 @@ namespace Coypu.AcceptanceTests.Examples
         }
 
         [Test]
-        public void ClickLink_example()
+        public void ClickButtonWaitBeforeClick()
+        {
+            var stopWatch = Stopwatch.StartNew();
+            Browser.ClickButton("clickMeTest", new Options {WaitBeforeClick = TimeSpan.FromMilliseconds(2000)});
+            var actualWait = stopWatch.ElapsedMilliseconds;
+            Console.WriteLine($"Actual wait before click {actualWait} milliseconds");
+            Assert.That(actualWait > 2000, "\tDidn't wait enough!");
+        }
+
+        [Test]
+        public void ClickLink()
         {
             Browser.ClickLink("Trigger a confirm");
             Browser.CancelModalDialog();
         }
 
         [Test]
-        public void ClickLinkWithTitle_example()
+        public void ClickLinkWithTitle()
         {
             Browser.ClickLink("Link with title");
             Browser.CancelModalDialog();
+        }
+
+        [Test]
+        public void ClickWaitBeforeClick()
+        {
+            var stopWatch = Stopwatch.StartNew();
+            var element = Browser.FindButton("clickMeTest");
+            element.Click(new Options {WaitBeforeClick = TimeSpan.FromMilliseconds(2000)});
+            var actualWait = stopWatch.ElapsedMilliseconds;
+            Console.WriteLine($"Actual wait before click {actualWait} milliseconds");
+            Assert.That(actualWait > 2000, "\tDidn't wait enough!");
+
+
+            //var waiter = new StopwatchWaiter();
+            //var stopWatch = Stopwatch.StartNew();
+            //var expectedDuration = TimeSpan.FromMilliseconds(expectedDurationMilliseconds);
+
+            //waiter.Wait(expectedDuration);
+
+            //var actualWait = stopWatch.ElapsedMilliseconds;
+
+            //const int toleranceMilliseconds = AccuracyMilliseconds;
+
+            //Assert.That((int)actualWait, Is.InRange(expectedDurationMilliseconds - toleranceMilliseconds,
+            //                                        expectedDurationMilliseconds + toleranceMilliseconds));
         }
     }
 }
