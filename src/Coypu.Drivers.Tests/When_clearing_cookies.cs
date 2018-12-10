82d3788122f6fafd1ae42ad69fabc11ffb748d83
Jakub Obstarczyk <jakub.obstarczyk@gmail.com>
2018-12-10 20:28:33 +0000

    adding tests for deleting cookies

--- src/Coypu.Drivers.Tests/When_clearing_cookies.cs
+++ src/Coypu.Drivers.Tests/When_clearing_cookies.cs
@@ -1,5 +1,6 @@
 using System.Linq;
 using NUnit.Framework;
+using OpenQA.Selenium;
 
 namespace Coypu.Drivers.Tests
 {
@@ -16,7 +17,7 @@ namespace Coypu.Drivers.Tests
         }
 
         [Test]
-        public void Clear_all_the_session_cookies()
+        public void Delete_all_the_session_cookies()
         {
             Driver.ExecuteScript("document.cookie = 'cookie1=value1; '", Root);
             Driver.ExecuteScript("document.cookie = 'cookie2=value2; '", Root);
@@ -34,6 +35,52 @@ namespace Coypu.Drivers.Tests
             Driver.Cookies.DeleteAll();
 
             Assert.AreEqual(0,
+                            Driver.Cookies.GetAll()
+                                  .Count());
+        }
+
+        [Test]
+        public void Delete_cookie()
+        {
+            var specialCookie = new Cookie("specialCookie", "specialValue");
+            Driver.Cookies.AddCookie(specialCookie);
+            Driver.ExecuteScript("document.cookie = 'cookie2=value2; '", Root);
+
+            var cookieCount = Driver.Cookies.GetAll()
+                                    .Count();
+            var expectedCookieCount = cookieCount - 1;
+
+            Driver.Cookies.DeleteCookie(specialCookie);
+            var cookies = Driver.Cookies.GetAll()
+                                .ToArray();
+
+            Assert.That(cookies, Does.Not.Contain(cookies.Any(c => c.Name == "specialCookie")));
+            Assert.That(cookies.First(c => c.Name == "cookie2")
+                               .Value,
+                        Is.EqualTo("value2"));
+            Assert.AreEqual(expectedCookieCount,
+                            Driver.Cookies.GetAll()
+                                  .Count());
+        }
+
+        [Test]
+        public void Delete_cookie_by_name()
+        {
+            Driver.ExecuteScript("document.cookie = 'cookie1=value1; '", Root);
+            Driver.ExecuteScript("document.cookie = 'cookie2=value2; '", Root);
+            var cookieCount = Driver.Cookies.GetAll()
+                                    .Count();
+            var expectedCookieCount = cookieCount - 1;
+
+            Driver.Cookies.DeleteCookieNamed("cookie1");
+            var cookies = Driver.Cookies.GetAll()
+                                .ToArray();
+
+            Assert.That(cookies, Does.Not.Contain(cookies.Any(c => c.Name == "cookie1")));
+            Assert.That(cookies.First(c => c.Name == "cookie2")
+                               .Value,
+                        Is.EqualTo("value2"));
+            Assert.AreEqual(expectedCookieCount,
                             Driver.Cookies.GetAll()
                                   .Count());
         }
